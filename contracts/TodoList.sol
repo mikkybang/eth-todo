a9a8724a7928ef898e8dadb4e8112d5a428b3e2e
Michael Efemena <mefemena@gmail.com>
2022-02-15 00:02:27 +0100

    added Task struct and mapping

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,13 @@
 pragma solidity ^0.8.11;
 
 contract TodoList {
-    uint public taskCount = 0;
-    
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) tasks;
 }
